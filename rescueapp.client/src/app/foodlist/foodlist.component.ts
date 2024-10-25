7b64cad8fdaa2b287153ebe16a05f75632418431
pasindu madubashana <madubashana.pasindu@gmail.com>
2024-10-24 22:54:44 -0400

    Added libraries

---
+++ rescueapp.client/src/app/foodlist/foodlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foodlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './foodlist.component.html',
+  styleUrl: './foodlist.component.scss'
+})
+export class FoodlistComponent {
+
+}
