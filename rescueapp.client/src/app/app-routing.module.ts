005fc2ac37335b124356e88ea639daa2cc654a79
pasindu madubashana <madubashana.pasindu@gmail.com>
2024-11-05 22:21:23 -0500

    Added routing

--- rescueapp.client/src/app/app-routing.module.ts
+++ rescueapp.client/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { AddFoodItemComponent } from './pages/restaurant-home/add-food-item.comp
 import { EditFoodItemComponent } from './pages/restaurant-home/edit-food-item.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FoodlistComponent } from './pages/foodlist/foodlist.component';
+import { OrdersComponent } from './pages/orders/orders.component';
 
 const routes: Routes = [
   {
@@ -50,6 +51,10 @@ const routes: Routes = [
       {
         path: 'foodlist',
         component: FoodlistComponent,
+      },
+      {
+        path: 'orders',
+        component: OrdersComponent,
       },
       {
         path: 'edit-food-item/:name',
