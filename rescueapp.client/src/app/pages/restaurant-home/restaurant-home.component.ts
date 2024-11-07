2f49efbbc51adef6af704401c92b5fd59db7356b
Eshan-m <thineemadura@gmail.com>
2024-11-07 14:22:01 -0500

    Bug fix in delete caused by merged

--- rescueapp.client/src/app/pages/restaurant-home/restaurant-home.component.ts
+++ rescueapp.client/src/app/pages/restaurant-home/restaurant-home.component.ts
@@ -13,29 +13,36 @@ export class RestaurantHomeComponent implements OnInit {
   totalItems = 0; // Will display the total number of food items
 
 
-  restaurantUsername: string = 'exa'; 
+  restaurantUsername: string = 'exa';
 
   constructor(private router: Router, private service: ServiceService) { }
 
   ngOnInit(): void {
     // Fetch food items (API call can be added here)
-  
-      this.loadFoodItems();
-    
-  
+
+    this.loadFoodItems();
+
+
   }
 
-  onDelete(item: { name: string; quantity: number; expiry: string; status: string }): void {
-    // Logic to delete item (API call can be added here)
-    console.log('Deleting item:', item);
+  onDelete(item: { name: string; quantity: number; expiry: string; status: string; id: number }): void {
+    console.log(item.id);
+    this.service.Deletefooditem(item.id).subscribe(
+      (items) => {
+        window.location.reload();
+      },
+      (error) => {
+        console.error('Error fetching food items', error);
+      }
+    );
   }
 
   loadFoodItems(): void {
-  
+
     this.service.GetfooditemsRes(localStorage.getItem("Username")?.toString()).subscribe(
       (items) => {
         this.foodItems = items;
-       
+
         // Update total items count
         console.log(this.totalItems = this.foodItems.length);
       },
